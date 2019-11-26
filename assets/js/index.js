88b8224e4e584c9d4df61443ef0c75c127dd4b44
RobinHabran <robinhabran@gmail.com>
2019-11-27 00:23:09 +0100

    overlay Actualité

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,34 @@
-// ouverture de l'overlay
-document.getElementById('actuality').onclick = function(){
+// fermeture du collapse lors du click n'importe ou
+$(function() {
+  $(document).click(function (event) {
+    $('.navbar-collapse').collapse('hide');
+  });
+});
+// ouverture de l'overlay au click sur la categorie
+document.getElementById('actuality').onclick = function openOverlay(){
   document.getElementById('overlayActuality').style.width = '100%';
 }
-// fermeture de l'overlay
-document.getElementsByClassName('closebtn').onclick = function(){
-  document.getElementById('overlayActuality').style.width = '0';
+// fermeture de l'overlay au click
+document.getElementById('closebtn').onclickfunction = function closeOverlay(){
+  document.getElementById('overlayActuality').style.width = '0%';
 }
 
-// Set the date we're counting down to
-var now = new Date().getTime();
-var year = new Date().getFullYear()
-var countDownDate = new Date("Jan 01, 2019 00:00:00").getTime();
 function deadRealTime() {
-  // Get today's date and time
-
+  // date
+  var now = new Date().getTime();
+  // année actuelle
+  var year = new Date().getFullYear();
+  // date de départ
+  var countDownDate = new Date("Jan 01, 2019 00:00:00").getTime();
+  // temps entre date départ et aujourd'hui
   var distance = -(countDownDate - now);
-   // Time calculations for days, hours, minutes and seconds
+  // statistiques du génocide
+  var deadPerDay = 69; var deadPerYear = 25185;
+   // calcul du temps pour: jours, heures, minutes and secondes
    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-   var deadPerDay = 69; var deadPerYear = 25185;
    // nombre de mort depuis le ...
    document.getElementById("timer").innerHTML = (days * deadPerDay) + '<span class="mort">morts</span>';
    document.getElementById("sentens").innerHTML = 'depuis le 1er janvier ' + year;
@@ -30,10 +37,3 @@ function deadRealTime() {
 deadRealTime();
 // actualisation toutes les minutes du compteur
 setInterval(deadRealTime,60000);
-
-// fermeture du collapse lors du click n'importe ou
-$(function() {
-  $(document).click(function (event) {
-    $('.navbar-collapse').collapse('hide');
-  });
-});
