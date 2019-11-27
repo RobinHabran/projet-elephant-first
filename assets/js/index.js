5d6e753d25400fec674aa9454780b4cedbf6d427
Robin.Habran <robinhabran@gmail.com>
2019-11-27 16:31:07 +0100

    timer fonctionnel

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,35 +5,55 @@ $(function() {
   });
 });
 // ouverture de l'overlay au click sur la categorie
-document.getElementById('actuality').onclick = function openOverlay(){
-  document.getElementById('overlayActuality').style.width = '100%';
+function openOverlay(){
+  // fermeture d'un ouverlay si ouvert
+  document.getElementById('overlay').style.width = '0%';
+  document.getElementById('overlay').style.width = '100%';
+  //document.getElementsByTagName('A').classList.remove('active');
+  document.getElementById('home').classList.remove('active');
+  document.getElementById('actuality').classList.add('active');
 }
 // fermeture de l'overlay au click
-document.getElementById('closebtn').onclickfunction = function closeOverlay(){
-  document.getElementById('overlayActuality').style.width = '0%';
+function closeOverlay(){
+  document.getElementById('overlay').style.width = '0%';
+  document.getElementById('actuality').classList.remove('active');
+  document.getElementById('home').classList.add('active');
 }
+// ouverture de la categorie 'actuality'
+document.getElementById('actuality').addEventListener('click',openOverlay);
+// retour à la categorie 'home'
+document.getElementById('home').addEventListener('click',closeOverlay);
+// fermeture des overlay
+document.getElementById('closebtn').addEventListener('click',closeOverlay);
 
-function deadRealTime() {
-  // date
-  var now = new Date().getTime();
-  // année actuelle
-  var year = new Date().getFullYear();
-  // date de départ
-  var countDownDate = new Date("Jan 01, 2019 00:00:00").getTime();
-  // temps entre date départ et aujourd'hui
-  var distance = -(countDownDate - now);
-  // statistiques du génocide
-  var deadPerDay = 69; var deadPerYear = 25185;
-   // calcul du temps pour: jours, heures, minutes and secondes
-   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-   // nombre de mort depuis le ...
-   document.getElementById("timer").innerHTML = (days * deadPerDay) + '<span class="mort">morts</span>';
-   document.getElementById("sentens").innerHTML = 'depuis le 1er janvier ' + year;
+// date
+var now = new Date().getTime();
+// année actuelle
+var year = new Date().getFullYear();
+// date de départ
+var countDownDate = new Date('Jan 01, '+year+' 00:00:00').getTime();
+// temps entre date départ et aujourd'hui
+var distance = -(countDownDate - now);
+// calcul du temps pour: jours, heures, minutes and secondes
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// statistiques du génocide
+var deadPerYear = 25185;
+var deadPerDay = Math.round(deadPerYear / 365);
+var deadPerHour = Math.round(deadPerDay / 24);
+var deadPerMinute = deadPerDay / 1440 ;
+// nombre de mort depuis le ...
+var deadPerDayToday = deadPerDay * days;
+document.getElementById('timer').innerHTML = deadPerDayToday + '<span class="mort">morts</span>';
+document.getElementById('sentens').innerHTML = 'depuis le 1er janvier ' + year;
+
+function refreshStat() {
+  document.getElementById('timer').innerHTML = deadPerDayToday + Math.trunc(deadPerHour + minutes * deadPerMinute) + '<span class="mort">morts</span>';
 }
 // premier appel du compteur
-deadRealTime();
+//deadRealTime();
 // actualisation toutes les minutes du compteur
-setInterval(deadRealTime,60000);
+
+setInterval(refreshStat(),100);
